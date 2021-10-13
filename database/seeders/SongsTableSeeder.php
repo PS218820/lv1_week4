92abd0378bc2f4a66fdd1b3362975932ff61607b
PS218820 <74542567+PS218820@users.noreply.github.com>
2021-10-13 10:18:09 +0200

    comit 5

--- database/seeders/SongsTableSeeder.php
+++ database/seeders/SongsTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate/Support/Facades/DB;
+use Carbon/carbon;
 
 class SongsTableSeeder extends Seeder
 {
@@ -13,10 +15,11 @@ class SongsTableSeeder extends Seeder
      */
     public function run()
     {
-      DB::table('createsongstable')->insert([
-          'name' => Str::random(10),
-          'email' => Str::random(10).'@gmail.com',
-          'password' => Hash::make('password'),
+      DB::table('songs')->insert([
+          'title' => 'Ik ga zwemmen',
+          'singer' => 'Mart Hoogkamer',
+          'created_at' => Carbon::now,
+          'updated_at' => Carbon::now'
       ]);
     }
 }
